dc323e251ba7409be15808f95a3d7e18178d979c
HyunsangJoo <joohs86@naver.com>
2022-06-04 13:34:01 +0900

    Update
    enemy setup completed

---
+++ Assets/Scripts/StateMachines/Enemy/EnemyBaseState.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyBaseState : State
+{
+    protected EnemyStateMachine stateMachine;
+
+    public EnemyBaseState(EnemyStateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+
+    
+}
---
+++ Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System;
+
+public class EnemyStateMachine : StateMachine
+{
+    
+    
+}
